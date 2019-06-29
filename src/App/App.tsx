e2db2362718e30a1bf897025a68e0ae6a53e3b3a
Nicholas Weaver <nickqw14@gmail.com>
2019-06-28 22:55:03 -0600

    Infinite scroll is fully functional. Centered grid

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
+import React, { Component, Suspense, lazy } from "react";
 import styles from "./app.module.scss";
 import Grid from "../Grid/containers/Grid";
 import Welcome from "../Welcome/containers/Welcome";
+import InfiniteScroll from "react-infinite-scroller";
 
 type State = {
-	response: {};
 	error: boolean;
 	images: [];
 	query: string;
@@ -13,26 +13,24 @@ type State = {
 	loading: boolean;
 	width: number;
 	height: number;
+	hasMore: boolean;
 };
 
 class App extends Component {
 	state: State = {
-		response: {},
 		error: false,
 		images: [],
 		page: 1,
 		perPage: 30,
-		query: "",
+		query: "javascript",
 		loading: false,
 		width: 0,
-		height: 0
+		height: 0,
+		hasMore: true
 	};
-	componentWillMount() {
-		this.callAPI();
-	}
 	// Make API call to Unsplash to get list Photos
 	callAPI = () => {
-		const { page, perPage } = this.state;
+		let { page, perPage } = this.state;
 		// Unsplash API
 		const url: string = `https://api.unsplash.com/search/photos?page=${page}&per_page=${perPage}&query=${
 			this.state.query
@@ -46,27 +44,31 @@ class App extends Component {
 				Authorization: `Client-ID ${clientID}`
 			}
 		};
-		this.setState({
-			loading: true
-		});
 		// Fetch the data from unsplash
 		fetch(url, options)
 			.then(response => {
 				if (response.status !== 200) {
 					this.setState({
-						response: `There was a problem, status code ${response.status}`
+						response: `There was a problem, status code ${response.status}`,
+						loading: false
 					});
 					return;
 				}
+				this.setState({
+					loading: true
+				});
 				response.json().then(data =>
 					this.setState({
-						response: data,
 						images: [...this.state.images, ...data.results],
-						loading: false
+						loading: false,
+						hasMore: true,
+						page: page += 1
 					})
 				);
 			})
-			.catch(err => this.setState({ error: true, response: err }));
+			.catch(err =>
+				this.setState({ error: true, response: err, loading: false })
+			);
 	};
 	// Gets value as user types into the input
 	handleChange = (value: any) => {
@@ -82,30 +84,36 @@ class App extends Component {
 		});
 		this.callAPI();
 	};
+
 	render() {
-		const { images, loading } = this.state;
+		const { images, loading, hasMore } = this.state;
 		return (
-			<div className={styles.App}>
-				{loading ? (
-					<span>Loading...</span>
-				) : (
-					<div className={styles.App}>
-						{images.length > 0 ? (
-							<Grid
-								onChange={this.handleChange}
-								onSubmit={this.handleSubmit}
-								loading={loading}
-								images={images}
-							/>
-						) : (
-							<Welcome
-								onChange={this.handleChange}
-								onSubmit={this.handleSubmit}
-							/>
-						)}
-					</div>
-				)}
-			</div>
+			<InfiniteScroll
+				pageStart={0}
+				loadMore={this.callAPI}
+				hasMore={hasMore}
+				loader={
+					loading && (
+						<div className="loader" key={0}>
+							Loading ...
+						</div>
+					)
+				}
+				useWindow={true}
+				initialLoad={true}
+				useCapture={true}
+				isReverse={false}
+				threshold={50}
+			>
+				<div className={styles.App}>
+					<Grid
+						images={images}
+						loading={loading}
+						onChange={this.handleChange}
+						onSubmit={this.handleSubmit}
+					/>
+				</div>
+			</InfiniteScroll>
 		);
 	}
 }
--- src/Loading/components/Loading.tsx
+++ src/Loading/components/Loading.tsx
@@ -1,54 +1,49 @@
-import * as React from 'react';
+import * as React from "react";
 
 type State = {
-	response: {}
-	error: boolean
-}
+	response: {};
+	error: boolean;
+};
 
 type Props = {
-	url: string
-	options?: {}
-	loading: boolean
-}
+	url: string;
+	options?: {};
+	loading: boolean;
+};
 class Loading extends React.Component<Props, State> {
-	state : State = {
+	state: State = {
 		response: {},
 		error: false
+	};
+	componentWillMount() {
+		this.callAPI();
 	}
 	callAPI = () => {
 		const { url, options } = this.props;
 		fetch(url, options)
-			.then(
-				response => {
-					if (response.status !== 200) {
-						this.setState({
-							response: `There was a problem, status code ${response.status}`
-						});
-						return;
-					}
-					response.json().then(
-						data => this.setState({
-							response: data
-						})
-					);
+			.then(response => {
+				if (response.status !== 200) {
+					this.setState({
+						response: `There was a problem, status code ${response.status}`
+					});
+					return;
 				}
-			)
-			.catch(
-				err => this.setState({error: true, response: err})
-			)
-	}
-  render() {
+				response.json().then(data =>
+					this.setState({
+						response: data
+					})
+				);
+			})
+			.catch(err => this.setState({ error: true, response: err }));
+	};
+	render() {
 		const { loading } = this.props;
-    return (
+		return (
 			<div>
-				{loading ?
-					<span>Loading...</span>
-					:
-					<button>Load More</button>
-				}
+				{loading ? <span>Loading...</span> : <button>Load More</button>}
 			</div>
-    );
-  }
+		);
+	}
 }
 
 export default Loading;
